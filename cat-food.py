8b78fc0d7e9836663d3a000479246191fb0ea0c5
Anza-Azam <anzaazam.nw4@gmail.com>
2022-03-26 19:43:17 +0000

    First Python program

--- cat-food.py
+++ cat-food.py
@@ -1,5 +1,5 @@
-chairs = 15
-nails = 4
-total_nails = chairs * nails
-message = 'I need to buy {} nails'.format(total_nails)
-print('You need to buy {} nails'.format(message))
+my_name = 'Penelope'
+my_age = 29
+message = 'My name is {} and I am {} years old'.format(my_name, my_age)
+print(message)
+
