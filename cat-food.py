0dad9120306be233e4e576c66ef96131a75f2a43
Anza-Azam <anzaazam.nw4@gmail.com>
2022-03-29 18:58:35 +0100

    int and input

--- cat-food.py
+++ cat-food.py
@@ -1,6 +1,3 @@
-box_of_eggs = 6
-eggs_for_omlette=4
-number_of_omlettes= (box_of_eggs*6)/eggs_for_omlette
-message = 'You can make {} omelettes with {} boxes of eggs'.format(number_of_omlettes,box_of_eggs)
-print(message)
-
+friends= input('how manu')
+pizza=int(friends)*0.5
+print(str(pizza))
