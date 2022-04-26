7270bd8a7d289df6d6b1dfac45015651d4dcd6b6
Anza-Azam <anzaazam.nw4@gmail.com>
2022-04-26 21:10:55 +0100

    int and input

--- cat-food.py
+++ cat-food.py
@@ -1,7 +1,78 @@
-place={'location':{'longitude':33}}
-a =place['location']
+import random
+import requests
 
-print(a['longitude'])
+your_name = input('What is your name?')
+
+names = ['Emma', 'Anza', 'Jess']
+computer_name = random.choice(names)
+
+def player_pokemon():
+    pokemon_number = random.randint(1, 151)
+    url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+    response = requests.get(url)
+    pokemon = response.json()
+    player = {
+        'name': pokemon['name'],
+        'id': pokemon['id'],
+        'height': pokemon['height'],
+        'weight': pokemon['weight'],
+    }
+    return player
+player1= player_pokemon();
+
+def computer_pokemon():
+    pokemon_number = random.randint(1, 151)
+    url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+    response = requests.get(url)
+    pokemon = response.json()
+    computer = {
+        'name': pokemon['name'],
+        'id': pokemon['id'],
+        'height': pokemon['height'],
+        'weight': pokemon['weight'],
+    }
+    return computer
+player2=computer_pokemon();
+
+def game():
+    print(your_name, 'picked {}'.format(player1['name']))
+    print("Pokemon id=", player1['id'])
+    print("Height=", player1['height'])
+    print("Weight=", player1['weight'])
+    stat_choice = input('Which stat do you want to use? (id, height, weight) ')
+
+    print("Your opponent", computer_name, "chose {}".format(player2['name']))
+    print(stat_choice, "=", player2[stat_choice])
+
+    my_stat = player1[stat_choice]
+    opponent_stat = player2[stat_choice]
+
+    if player1[stat_choice] > player2[stat_choice] :
+        print('You win')
+    elif player1[stat_choice] < player2[stat_choice]:
+        print('Better luck next time!')
+    else:
+        print("It's a draw!")
+
+
+game()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
