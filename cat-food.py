317b51125db1398d7f6dc60423ff291ec3886760
Anza-Azam <anzaazam.nw4@gmail.com>
2022-05-03 20:22:25 +0100

    int and input

--- cat-food.py
+++ cat-food.py
@@ -5,6 +5,8 @@ from termcolor import colored
 your_name = input('What is your name?')
 names = ['Emma', 'Anza', 'Jess']
 computer_name = random.choice(names)
+print("You are playing {}".format(computer_name))
+print()
 player_score = 0
 computer_score = 0
 
@@ -40,26 +42,26 @@ def game():
     global player_score, computer_score
     player1 = player_pokemon()
     player2 = computer_pokemon()
-    print(your_name, 'picked {}'.format(player1['name']))
+    print("{} 'picks {}".format(your_name, player1['name']))
     print("Pokemon id=", player1['id'])
     print("Height=", player1['height'])
     print("Weight=", player1['weight'])
     stat_choice = input('Which stat do you want to use? (id, height, weight) ')
-
-    print("Your opponent", computer_name, "chose {}".format(player2['name']))
+    print()
+    print("Your opponent {} picks {}".format(computer_name, player2['name']))
     print(stat_choice, "=", player2[stat_choice])
-
+    print()
     my_stat = player1[stat_choice]
     opponent_stat = player2[stat_choice]
 
     if player1[stat_choice] > player2[stat_choice]:
-        print('You win')
+        print(colored("You win", 'green'))
         player_score = player_score + 1
     elif player1[stat_choice] < player2[stat_choice]:
-        print('Better luck next time!')
+        print(colored("You lose! Better luck next time!", 'red'))
         computer_score = computer_score + 1
     else:
-        print("It's a draw!")
+        prin(colored("It's a draw!", 'blue'))
 
 
 
@@ -67,14 +69,16 @@ no_of_games = 0
 
 while no_of_games < 3:
     game()
-
     no_of_games = no_of_games + 1
-    print(colored("The outcome!", attrs=['bold']))
-    print("{}'s score is: {}".format(your_name, player_score))
-    print("{}'s score is: {}".format(computer_name, computer_score))
-    if player_score < computer_score:
-        print("Oh no, {}!! You have lost!".format(computer_name, your_name))
-    elif player_score > computer_score:
-        print("Congratulations, {}!! You have won!".format(your_name))
-    else:
-        print("You have tied!")
+    print()
+    print(colored("{}'s score is: {}".format(your_name, player_score), 'blue'))
+    print(colored("{}'s score is: {}".format(computer_name, computer_score), 'blue'))
+    print()
+    print()
+
+if player_score > computer_score:
+    print(colored("Congratulations {}!!!, you have won!".format(your_name), 'green'))
+elif player_score < computer_score:
+    print(colored("Oh no {}!!! You have lost".format(your_name), 'red'))
+else:
+    print(colored("You have tied", 'blue'))
