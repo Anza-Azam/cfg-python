a05e3dc988909fd35f27c8d3f164fc6d94b8f0e3
Anza-Azam <anzaazam.nw4@gmail.com>
2022-05-03 20:11:26 +0100

    int and input

--- cat-food.py
+++ cat-food.py
@@ -1,10 +1,12 @@
 import random
 import requests
+from termcolor import colored
 
 your_name = input('What is your name?')
-
 names = ['Emma', 'Anza', 'Jess']
 computer_name = random.choice(names)
+player_score = 0
+computer_score = 0
 
 def player_pokemon():
     pokemon_number = random.randint(1, 151)
@@ -18,7 +20,6 @@ def player_pokemon():
         'weight': pokemon['weight'],
     }
     return player
-player1= player_pokemon();
 
 def computer_pokemon():
     pokemon_number = random.randint(1, 151)
@@ -32,9 +33,13 @@ def computer_pokemon():
         'weight': pokemon['weight'],
     }
     return computer
-player2=computer_pokemon();
+
+
 
 def game():
+    global player_score, computer_score
+    player1 = player_pokemon()
+    player2 = computer_pokemon()
     print(your_name, 'picked {}'.format(player1['name']))
     print("Pokemon id=", player1['id'])
     print("Height=", player1['height'])
@@ -47,34 +52,29 @@ def game():
     my_stat = player1[stat_choice]
     opponent_stat = player2[stat_choice]
 
-    if player1[stat_choice] > player2[stat_choice] :
+    if player1[stat_choice] > player2[stat_choice]:
         print('You win')
+        player_score = player_score + 1
     elif player1[stat_choice] < player2[stat_choice]:
         print('Better luck next time!')
+        computer_score = computer_score + 1
     else:
         print("It's a draw!")
 
 
-game()
 
+no_of_games = 0
 
+while no_of_games < 3:
+    game()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    no_of_games = no_of_games + 1
+    print(colored("The outcome!", attrs=['bold']))
+    print("{}'s score is: {}".format(your_name, player_score))
+    print("{}'s score is: {}".format(computer_name, computer_score))
+    if player_score < computer_score:
+        print("Oh no, {}!! You have lost!".format(computer_name, your_name))
+    elif player_score > computer_score:
+        print("Congratulations, {}!! You have won!".format(your_name))
+    else:
+        print("You have tied!")
